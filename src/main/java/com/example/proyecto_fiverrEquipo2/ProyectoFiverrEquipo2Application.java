d83fa740485fc91171d5c1d919e2decf93e85e03
Laia <laia@hidalgojimenez.es>
2021-11-13 11:43:01 +0100

    añadir proyecto

---
+++ src/main/java/com/example/proyecto_fiverrEquipo2/ProyectoFiverrEquipo2Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_fiverrEquipo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFiverrEquipo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFiverrEquipo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_fiverrEquipo2/ProyectoFiverrEquipo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_fiverrEquipo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFiverrEquipo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
